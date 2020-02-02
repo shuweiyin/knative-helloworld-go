093337395dfbf22e85ba3a793888956e2d12e94e
shuwei.yin <shuwei.yin@alibaba-inc.com>
2020-02-02 10:54:28 +0000

    update test eventing
    update test eventing

--- helloworld.go
+++ helloworld.go
@@ -3,17 +3,17 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
 	"time"
-	"math/rand"
 )
 
 type Log struct {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	l:= &Log{}
+	l := &Log{}
 	l.Info("Hello world received a request.")
 	target := os.Getenv("TARGET")
 	if target == "" {
@@ -22,7 +22,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 	rand.Seed(time.Now().Unix())
 
-	fmt.Fprintf(w, "Hello knative from tekten %s!\n", target)
+	fmt.Fprintf(w, "Hello knative  tekten %s!\n", target)
 
 	if rand.Intn(10) < 4 {
 		l.Error("http handler error")
@@ -33,7 +33,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	l:= &Log{}
+	l := &Log{}
 	l.Info("Hello world sample started.")
 
 	http.HandleFunc("/", handler)
@@ -48,33 +48,32 @@ func main() {
 	}
 }
 
-func (log *Log) Infof(format string, a ...interface{})  {
+func (log *Log) Infof(format string, a ...interface{}) {
 	log.log("INFO", format, a...)
 }
 
-func (log *Log) Info(msg string)  {
+func (log *Log) Info(msg string) {
 	log.log("INFO", "%s", msg)
 }
 
-func (log *Log) Errorf(format string, a ...interface{})  {
+func (log *Log) Errorf(format string, a ...interface{}) {
 	log.log("ERROR", format, a...)
 }
 
-func (log *Log) Error(msg string)  {
+func (log *Log) Error(msg string) {
 	log.log("ERROR", "%s", msg)
 }
 
-func (log *Log) Fatalf(format string, a ...interface{})  {
+func (log *Log) Fatalf(format string, a ...interface{}) {
 	log.log("FATAL", format, a...)
 }
 
-func (log *Log) Fatal(msg string)  {
+func (log *Log) Fatal(msg string) {
 	log.log("FATAL", "%s", msg)
 }
 
-func (log *Log) log(level, format string, a ...interface{})  {
+func (log *Log) log(level, format string, a ...interface{}) {
 	var cstSh, _ = time.LoadLocation("Asia/Shanghai")
 	ft := fmt.Sprintf("%s %s %s\n", time.Now().In(cstSh).Format("2006-01-02 15:04:05"), level, format)
 	fmt.Printf(ft, a...)
 }
-
